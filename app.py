bc0fa0c59b290df93bb4afb7255ddff067d56523
TheSlimyOne <70922587+TheSlimyOne@users.noreply.github.com>
2023-03-18 15:06:31 -0400

    Updated GUI

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
 import cv2
 import math
 
-# Make sure to put the image in the Maps Folder
-img = cv2.imread("Maps\\" + input("Enter the name of the file (make sure the file is in the Maps folder): "), 0)
+def calculate(ocean=True):
+    try:
+        img = cv2.imread(input("\nEnter the path to the file: "), 0)
+        rows, cols = img.shape
+    except Exception:
+        print("\n--- Image was invalid ---\n")
+        return
+    
+    img_size = rows * cols
 
-rows, cols = img.shape
-img_size = rows * cols
+    land_pixels = cv2.countNonZero(img) # Counts all pixels that are not black
+    ocean_pixels = img_size - land_pixels
 
-land_pixels = cv2.countNonZero(img) # Counts all pixels that are not black
-ocean_pixels = img_size - land_pixels
+    print(f"Image size is {rows}px x {cols}px or {img_size}px")
+    print(f"There are {land_pixels} pixels of land, {ocean_pixels} pixels of ocean")
 
-print(f"Image size is {rows}px x {cols}px or {img_size}px")
-print(f"There are {land_pixels} pixels of land, {ocean_pixels} pixels of ocean")
-print(f"This map is comprised of {round(ocean_pixels/img_size * 100, 2)}% Ocean")
+    dividend = ocean_pixels if ocean else land_pixels
+    name = "Ocean" if ocean else "Land"
+
+    print(f"This map is comprised of {round(dividend/img_size * 100, 2)}% {name}")
+
+if __name__ == "__main__":
+    while True:
+        print("-----------------------------")
+        print("1. Calculate ocean percentage")
+        print("2. Calculate land percentage")
+        print("3. Quit")
+        print("-----------------------------")
+        selection = input("Choose a number: ")
+
+
+        if selection == "1":
+            calculate()
+        elif selection == "2":
+            calculate(False)
+        elif selection == "3":
+            exit()
+        else:
+            print("invalid input try again!")
